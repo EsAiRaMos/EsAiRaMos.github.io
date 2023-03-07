01946593ea969a3e0ccce1b96e35d346b040699c
Esai C. Ramos <esaramos@uat.edu>
2023-03-07 02:59:14 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom'; // add import for HashRouter
 import './index.css';
 import App from './App';
