6d9afa8160c5495046bf7309609f36853ac4b994
Esai C. Ramos <esaramos@uat.edu>
2023-03-07 03:16:10 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
+//index. js 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // add import for HashRouter
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
-const root = document.getElementById('root'); // remove the ReactDOM.render call
+const root = ReactDOM.render(document.getElementById('root'));
 ReactDOM.render(
-<React.StrictMode>
-<HashRouter>
-<App />
-</HashRouter>
-</React.StrictMode>,
-root // render the App component to the root element
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
