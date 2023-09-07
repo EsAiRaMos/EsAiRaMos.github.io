696aeabd8ee9779d229367f3c6c6abcbf75216b7
Esai C. Ramos <esaramos@uat.edu>
2023-09-06 21:50:46 -0700

    new stuff

--- src/Screens/Homepage/Home.js
+++ src/Screens/Homepage/Home.js
@@ -1,57 +1,66 @@
-import React from 'react';
-import './Home.scss'; //imports the scss styles
-import myImage from '../../Assets/images/optimized_IMG_9428.svg';
+import React, { useState } from 'react';
+import './Home.scss'; // imports the SCSS styles
+import myImage from '../../Assets/images/optimized-svg.svg';
+
+const navigationItems = [
+  { label: 'Home', content: 'Welcome to my homepage.' },
+  { label: 'About Me', content: 'Currently enrolled in University of Advancing Technology. I\'m majoring in Advancing Computer Science.' },
+  { label: 'Skills', content: 'Here are some of my skills.' },
+  { label: 'Projects', content: 'Here are some of my projects below.' },
+  { label: 'Contacts', content: 'Contact me at example@email.com.' },
+];
 
 function Home() {
-    return (
-        <div className='website-container'>
+  const [content, setContent] = useState(''); // State to track the content
 
-            
-            <section className='nav-container'>
-                <nav>
-                    <header>
-                        <h2>WELCOME |</h2>
-                    </header>
-                    <main>
-                        <h3>Home |</h3>
-                        <h3>About Me |</h3>
-                        <h3>Skills |</h3>
-                        <h3>Projects |</h3>
-                        <h3>Contacts |</h3>
-                    </main>
-                </nav>
-            </section>
+  const handleNavItemHover = (newContent) => {
+    setContent(newContent); // Update the content when hovering over nav items
+  };
 
-            <section className='main-container'>
-                <header>
-                    <h1>My name is Esai C. Ramos. I'm a UI/UX Designer.</h1>
-                    <p>This is my personal website showcasing my various projects.</p>
-                </header>
-                <main>
-                    <section>
-                        <h2>About Me</h2>
-                        <p>Currently enrolled in University of Advancing Technology. I'm majoring in Advancing Computer Science.</p>
-                    </section>
-                    <section>
-                        <h2>Projects</h2>
-                        <p>Here are some of my projects below.</p>
-                    </section>
-                </main>
+  return (
+    <div className='website-container'>
+      <section className='nav-container'>
+        <nav>
+          <main>
+            {navigationItems.map((item, index) => (
+              <h3
+                key={index}
+                onMouseEnter={() => handleNavItemHover(item.label)}
+              >
+                {item.label}
+              </h3>
+            ))}
+          </main>
+        </nav>
+      </section>
 
-                <footer>
-                    <p>&copy; 2023 My website. All rights reserved.</p>
-                </footer>
-            </section>
+      <section className='main-container'>
+        <header>
+          <h1>My name is Esai C. Ramos. I'm a UI/UX Designer.</h1>
+          <p>This is my personal website showcasing my various projects.</p>
+        </header>
+        <main>
+          {navigationItems.map((item, index) => (
+            content === item.label && (
+              <section key={index}>
+                <h2>{item.label}</h2>
+                <p>{item.content}</p>
+              </section>
+            )
+          ))}
+        </main>
 
-            <section className='esai-container'>
-            <caption>
-                <img src={myImage} alt="Esai Ramos" />
-                    <h2>Esai Ramos<br />Email | esaicr20@gmail.com<br/>Computer Science Major</h2>
-                </caption>
-            </section>
+        <footer>
+          <p>&copy; 2023 My website. All rights reserved.</p>
+        </footer>
+      </section>
 
-        </div>
-    );
+      <section className='esai-container'>
+        <img src={myImage} alt='Esai Ramos' />
+      </section>
+    </div>
+  );
 }
 
 export default Home;
+
